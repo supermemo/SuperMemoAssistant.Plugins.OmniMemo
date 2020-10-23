b507cb6f6cab5004472e4770c9ff417240ed5da5
Alexis <alexis@incogito.org>
2020-10-23 14:21:42 +0200

    - Added: Initial version

---
+++ src/SuperMemoAssistant.Plugins.OmniMemo/OmniMemoPlugin.cs
@@ -0,0 +1,117 @@
+﻿#region License & Metadata
+
+// The MIT License (MIT)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+
+
+
+namespace SuperMemoAssistant.Plugins.OmniMemo
+{
+  using System.Runtime.Remoting;
+  using System.Windows;
+  using System.Windows.Input;
+  using Anotar.Serilog;
+  using Interop.SuperMemo.Content.Controls;
+  using Interop.SuperMemo.Core;
+  using Services;
+  using Services.IO.Keyboard;
+  using Services.Sentry;
+  using SuperMemoAssistant.Extensions;
+  using Sys.IO.Devices;
+  using Views;
+
+  // ReSharper disable once ClassNeverInstantiated.Global
+  public class OmniMemoPlugin : SentrySMAPluginBase<OmniMemoPlugin>
+  {
+    #region Properties & Fields - Non-Public
+
+    private OmniMemoWindow MainWindow { get; set; }
+
+    #endregion
+
+
+
+
+    #region Constructors
+
+    public OmniMemoPlugin() : base("https://a63c3dad9552434598dae869d2026696@sentry.io/1362046") { }
+
+    #endregion
+
+
+
+
+    #region Properties Impl - Public
+
+    /// <inheritdoc />
+    public override string Name => "OmniMemo";
+
+    public override bool HasSettings => false;
+
+    #endregion
+
+
+
+
+    #region Methods Impl
+
+    protected override void PluginInit()
+    {
+      Svc.HotKeyManager
+         .RegisterGlobal(
+           "ShowOmniMemo",
+           "Show OmniMemo",
+           HotKeyScopes.Global,
+           new HotKey(Key.F, KeyModifiers.AltShift),
+           ShowOmniMemo);
+
+      MainWindow = new OmniMemoWindow();
+      Application.Current.MainWindow = MainWindow;
+    }
+
+    #endregion
+
+
+
+
+    #region Methods
+
+    [LogToErrorOnException]
+    public void ShowOmniMemo()
+    {
+      Application.Current.Dispatcher.InvokeAsync(() => MainWindow.ShowAndActivate());
+    }
+
+    [LogToErrorOnException]
+    public void OnElementChanged(SMDisplayedElementChangedEventArgs e)
+    {
+      try
+      {
+        IControlHtml ctrlHtml = Svc.SM.UI.ElementWdw.ControlGroup.GetFirstHtmlControl();
+      }
+      catch (RemotingException) { }
+    }
+
+    #endregion
+  }
+}
